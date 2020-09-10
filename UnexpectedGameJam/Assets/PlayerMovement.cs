6473f5f195edbe6a14b1060bb937bdb837095456
Adam-Higgins <adamskeee@hotmail.co.uk>
2020-09-10 14:09:50 +0100

    Basic Movement and placeholder model
    added in some basic 3rd person movement and added a placeholder model for the character to potentially get animations working

---
+++ UnexpectedGameJam/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    [SerializeField]
+    private float moveSpeed = 100;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+    }
+}
