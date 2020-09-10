c3210bf0b4de06ffae9d246799b7e5493496f598
robbiepeden1701176 <63721056+robbiepeden1701176@users.noreply.github.com>
2020-09-10 18:15:36 +0100

    Enemy Script
    Teleport script and archer follow script added

---
+++ UnexpectedGameJam/Assets/Scripts/Teleport.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public int Timer = 100;
+    public int Choice;
+    public float[] Position;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer = Timer - 1;
+
+        if (Timer == 0)
+        {
+            Move();
+        }
+
+    }
+
+    void Move()
+    {
+        Choice = Random.Range(0, 4);
+        Debug.Log("Yup");
+        Timer = 100;
+        transform.position = new Vector3(Position[Choice], 1, -Position[Choice]);
+    }
+}
