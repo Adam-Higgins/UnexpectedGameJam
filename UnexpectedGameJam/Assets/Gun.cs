064ffbe9ecca9f7b95af7005edcbee2cb95901b7
Adam-Higgins <adamskeee@hotmail.co.uk>
2020-09-11 13:17:47 +0100

    added in working a gun system with working rays
    rays are now cast from the camera instead of the player and a reticle has been added in so the player can now aim. also added in is a muzzle flash particle system

---
+++ UnexpectedGameJam/Assets/Gun.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 2)]
+    private float fireRate = 1;
+
+    [SerializeField]
+    private Transform firePoint;
+
+    public ParticleSystem muzzleFlash;
+
+    [SerializeField]
+    [Range(1,10)]
+    private int damage;
+
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= fireRate)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                timer = 0f;
+                FireGun();
+            }
+        }
+    }
+
+    private void FireGun()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
+
+        Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red, 10f);
+        //Ray ray = new Ray(firePoint.position, firePoint.forward);
+        RaycastHit hitInfo;
+
+        muzzleFlash.Play();
+
+        if (Physics.Raycast(ray, out hitInfo,100))
+        {
+            Debug.Log("Hit!");
+        }
+    }
+}
