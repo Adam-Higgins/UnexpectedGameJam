5d67c94a4dceee373a3242f20f48329da73eb264
Adam-Higgins <adamskeee@hotmail.co.uk>
2020-09-11 19:39:58 +0100

    moved all things from my branch manually due to merge conflicts
    everything from my branch that was working should now be working on the master

--- UnexpectedGameJam/Assets/Scripts/FollowMouseCamera.cs
+++ UnexpectedGameJam/Assets/Scripts/FollowMouseCamera.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class FollowMouseCamera : MonoBehaviour
 {
     public GameObject target;
-    public float rotateSpeed = 5;
+    public float rotateSpeedX = 5;
+    public float rotateSpeedY = 2;
     float horizontal;
+    float yaw;
 
     Vector3 offset;
 
@@ -19,13 +21,16 @@ public class FollowMouseCamera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        Cursor.lockState = CursorLockMode.Locked;
+        horizontal = Input.GetAxis("Mouse X") * rotateSpeedX;
         target.transform.Rotate(0, horizontal, 0);
 
         float desiredAngle = target.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
         transform.position = target.transform.position - (rotation * offset);
-        transform.LookAt(target.transform);
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeedY;
+        yaw += vertical;
+        yaw = Mathf.Clamp(yaw,-30,30);
+        transform.LookAt(new Vector3(target.transform.forward.x * 100,(target.transform.forward.y * 100) + yaw, target.transform.forward.z * 100));
     }
 }
---
+++ UnexpectedGameJam/Assets/Scripts/Gun.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 2)]
+    private float fireRate = 1;
+
+    [SerializeField]
+    private Transform firePoint;
+
+    public ParticleSystem muzzleFlash;
+
+    [SerializeField]
+    [Range(1,10)]
+    private int damage;
+
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= fireRate)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                timer = 0f;
+                FireGun();
+            }
+        }
+    }
+
+    private void FireGun()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.DrawRay(ray.origin, RotatePointAroundPivot(ray.direction, ray.origin, new Vector3(Random.Range(-0.5f,0.5f), Random.Range(-0.5f, 0.5f), 0)) * 100f, Color.red, 10f);
+        }
+        Debug.DrawRay(ray.origin, RotatePointAroundPivot(ray.direction,ray.origin, new Vector3(1f,1f,0)) * 100f , Color.red, 10f);
+       // Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red, 10f);
+      //  Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red, 10f);
+      //  Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red, 10f);
+     //   Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red, 10f);
+      //  Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red, 10f);
+        RaycastHit hitInfo;
+
+        muzzleFlash.Play();
+
+        if (Physics.Raycast(ray, out hitInfo,100))
+        {
+            Debug.Log("Hit!");
+        }
+    }
+    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    {
+        Vector3 dir = point - pivot; // get point direction relative to pivot
+        dir = Quaternion.Euler(angles) * dir; // rotate it
+        point = dir + pivot; // calculate rotated point
+        return point; // return it
+    }
+}
